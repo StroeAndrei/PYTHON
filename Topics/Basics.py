8a92fbda3af11c27e3bc345184ab85c57bf6da05
Andrei <str0e_andrei@yahoo.com>
2020-09-14 11:51:23 +0300

    11:51

---
+++ Topics/Basics.py
@@ -0,0 +1,20 @@
+# variable declaration
+message = "Hello"
+
+# output the value of a variable
+print(message)
+
+# output with uppercase letters
+print(message.upper())
+
+# output with lowercase letters
+print(message.lower())
+
+# output with first letter uppercase
+print(message.title())
+
+# string concatenation
+# we use "+" to concatenate strings
+print("Hello" + "!!!")
+
+# OBS: if we want to output numbers as strings we use str() function
